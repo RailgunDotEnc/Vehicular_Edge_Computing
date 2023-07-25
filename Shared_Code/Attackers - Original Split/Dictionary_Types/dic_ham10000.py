e9977159c36648f3b871dde4b11a4e61f8674838
Roy <slrmbb@umsystem.edu>
2023-07-25 14:17:44 -0500

    aaa

---
+++ Shared_Code/Attackers - Original Split/Dictionary_Types/dic_ham10000.py
@@ -0,0 +1,11 @@
+DATA_NAME="ham10000"
+NUM_CHANNELS=3
+IMG_TYPE = {
+    'nv': 'Melanocytic nevi',
+    'mel': 'Melanoma',
+    'bkl': 'Benign keratosis-like lesions ',
+    'bcc': 'Basal cell carcinoma',
+    'akiec': 'Actinic keratoses',
+    'vasc': 'Vascular lesions',
+    'df': 'Dermatofibroma'
+}
