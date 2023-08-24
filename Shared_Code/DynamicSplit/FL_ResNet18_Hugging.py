22632cd55de4a667b1802dfc528b0dab80ebbe06
RailgunDotEnc <daniel1120morandi@gmail.com>
2023-08-24 13:38:29 -0500

    Updated federated learning to use googlenet

--- Shared_Code/DynamicSplit/FL_ResNet18_Hugging.py
+++ Shared_Code/DynamicSplit/FL_ResNet18_Hugging.py
@@ -47,7 +47,7 @@ import time
 from datetime import date, datetime
 today = f"{date.today()}".replace("-","_")
 timeS=f"{datetime.now().strftime('%H:%M:%S')}".replace(":","_")
-program=os.path.basename(__file__)+"_"+today+"_"+timeS+DATA_NAME
+program="FL"+ModelType+"_D"+today+"_T"+timeS+DATA_NAME+f"_U{NUM_USERS}_E{EPOCHS}_e{LOCAL_EP}.xlsx"
 TsArray=[]
 TcArray=[]
 
@@ -64,7 +64,7 @@ if torch.cuda.is_available():
 #===================================================================  
 
 print(f"---------{program}----------")              # this is to identify the program in the slurm outputs files
-
+program = f"Results\\{program}"
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # To print in color during test/train 
@@ -183,13 +183,13 @@ print(df.head())
 imageid_path = {os.path.splitext(os.path.basename(x))[0]: x
                 for x in glob(os.path.join("data", f'*({DATA_NAME})', '*.jpg'))}
 
-
+if DATA_NAME=="ImageNet":
+        for i in range(len(df["image_id"])):
+            df["image_id"][i]=str(df["image_id"][i])
 #print("path---------------------------------------", imageid_path.get)
 df['path'] = df['image_id'].map(imageid_path.get)
 df['cell_type'] = df[' fine_label'].map(IMG_TYPE.get)
 df['target'] = pd.Categorical(df['cell_type']).codes
-print(df['cell_type'].value_counts())
-print(df['target'].value_counts())
 #==============================================================
 # Custom dataset prepration in Pytorch format
 class SkinData(Dataset):
@@ -372,16 +372,102 @@ class ResNet18(nn.Module):
         x = self.fc(x)
 
         return x
+    
+#Building blocks for google net 
+class Inception_block(nn.Module):
+    def __init__(self, in_channels, out_1x1, red_3x3, out_3x3, red_5x5, out_5x5, out_1x1pool):
+        super(Inception_block, self).__init__()
+        
+        self.branch1 = conv_block(in_channels, out_1x1, kernel_size = 1)
+        self.branch2 = nn.Sequential(conv_block(in_channels, red_3x3, kernel_size=1), 
+                                     conv_block(red_3x3, out_3x3, kernel_size =3, stride=1, padding =1)
+                                     )
+        self.branch3 = nn.Sequential(
+            conv_block(in_channels, red_5x5, kernel_size=1),
+            conv_block(red_5x5, out_5x5, kernel_size=5, stride=1, padding=2)
+            )
+
+        self.branch4 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
+            conv_block(in_channels, out_1x1pool, kernel_size=1)
+            )
+
+    def forward(self, x):
+        #N x filters x 28 x 28
+        return torch.cat([self.branch1(x), self.branch2(x),self.branch3(x),self.branch4(x)], 1)
 
 
+class conv_block(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super(conv_block, self).__init__()
+        self.relu = nn.ReLU()
+        self.conv = nn.Conv2d(in_channels, out_channels, **kwargs)
+        self.batchnorm = nn.BatchNorm2d(out_channels)
+        
+    def forward(self, x):
+        return self.relu(self.batchnorm(self.conv(x)))
+    
+class GoogLeNetClient(nn.Module):
+    def __init__(self, in_channels=3, num_classes=1000,conv_block=None,Inception_block=None):
+        super(GoogLeNetClient, self).__init__()
+        self.conv1 = conv_block(in_channels=in_channels, out_channels = 64, kernel_size=7, stride =2, padding = 3)
+        
+        self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.conv2 = conv_block(64, 192, kernel_size =3, stride=1, padding=1)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.inception3a = Inception_block(192, 64, 96, 128, 16, 32, 32)
+        self.inception3b = Inception_block(256, 128, 128, 192, 32, 96, 64)
+        self.maxpool3 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        
+        self.inception4a = Inception_block(480, 192, 96, 208, 16, 48, 64)
+        self.inception4b = Inception_block(512, 160, 112, 224, 24, 64, 64)
+        self.inception4c = Inception_block(512, 128, 128, 256, 24, 64, 64)
+        self.inception4d = Inception_block(512, 112, 144, 288, 32, 64, 64)
+        self.inception4e = Inception_block(528, 256, 160, 320, 32, 128, 128)
+        self.maxpool4 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.inception5a = Inception_block(832, 256, 160, 320, 32, 128, 128)
+        self.inception5b = Inception_block(832, 384, 192, 384, 48, 128, 128)
+        
+        self.avgpool = nn.AvgPool2d(kernel_size=2, stride=1)
+        self.dropout = nn.Dropout(p=0.4)
+        self.fc1 = nn.Linear(1024,num_classes)
+        
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.maxpool1(x)
+        x = self.conv2(x)
+        x = self.maxpool2(x)
+        #Layer3
+        x = self.inception3a(x)
+        x = self.inception3b(x)
+        x = self.maxpool3(x)
+        x = self.inception4a(x)
+        x = self.inception4b(x)
+        x = self.inception4c(x)
+        x = self.inception4d(x)
+        x = self.inception4e(x)
+        x = self.maxpool4(x)
+        x = self.inception5a(x)
+        x = self.inception5b(x)
+        x = self.avgpool(x)
+        x = x.reshape(x.shape[0],-1)
+        x = self.dropout(x)
+        x = self.fc1(x)
+        return x
 
-net_glob = ResNet18(BasicBlock, RESNETTYPE, len(IMG_TYPE)) #7 is my numbr of classes
+
+if ModelType=="ResNet18":
+    print("Running ResNet")
+    net_glob = ResNet18(BasicBlock, RESNETTYPE, len(IMG_TYPE)) #7 is my numbr of classes
+elif ModelType=="GoogleNet":
+    print("Running GoogleNet")
+    net_glob =GoogLeNetClient(NUM_CHANNELS,len(IMG_TYPE),conv_block,Inception_block)
 if torch.cuda.device_count() > 1:
     print("We use",torch.cuda.device_count(), "GPUs")
     net_glob = nn.DataParallel(net_glob)   # to use the multiple GPUs 
 
 net_glob.to(device)
-print(net_glob)      
 
 #===========================================================================================
 # Federated averaging: FedAvg
@@ -465,8 +551,7 @@ print("Training and Evaluation completed!")
 # Save output data to .excel file (we use for comparision plots)
 round_process = [i for i in range(1, len(acc_train_collect)+1)]
 df = DataFrame({'round': round_process,'acc_train':acc_train_collect, 'acc_test':acc_test_collect, 'Gobal E Time (m)':TsArray, 'Local e Time per Client (m)': TcArray})     
-file_name = f"Results\\{program}_{NUM_USERS}.xlsx"
-df.to_excel(file_name, sheet_name= "v1_test", index = False)     
+df.to_excel(program, sheet_name= "v1_test", index = False)     
 
 #=============================================================================
 #                         Program Completed
